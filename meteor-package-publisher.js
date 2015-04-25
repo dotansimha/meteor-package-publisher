844bb5d62f938c63497963df4c7adefd89285ab2
Dotan Simha <dotansimha@gmail.com>
2015-04-25 16:31:42 +0300

    Working publisher :)

---
+++ meteor-package-publisher.js
@@ -0,0 +1,37 @@
+var argv = require('minimist')(process.argv.slice(2));
+var fs = require('fs');
+var MeteorPublisher = require('./lib/publisher');
+
+var packageInfoFilename = argv._[0];
+var publisher = new MeteorPublisher();;
+
+fs.readFile(packageInfoFilename, 'utf8', function (err, data) {
+    if (err) {
+        console.log('Error: ' + err);
+        return;
+    }
+
+    data = JSON.parse(data);
+
+    publisher.setDestination(data.destination);
+
+    var sourceParser = publisher.analyse(data);
+
+    sourceParser.clean();
+
+    sourceParser.load(data.source.name, data.source.version).then(function(sourceData) {
+        publisher.setSource(sourceData);
+
+        try {
+            publisher.test();
+            publisher.createPackageFile().then(function() {
+                publisher.publish().then(function() {
+                    publisher.clean();
+                })
+            });
+        }
+        catch (error) {
+            console.error(error);
+        }
+    });
+});
